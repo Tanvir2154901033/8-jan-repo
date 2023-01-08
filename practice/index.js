e7022d950d426cedd3772305ccab31be83710263
Tanvir2154901033 <mdtanvir3571@gmail.com>
2023-01-08 11:33:45 +0600

    repo

---
+++ practice/index.js
@@ -0,0 +1,6 @@
+var btn= document.querySelector("#btn");
+var div1= document.querySelector(".div1");
+var h1=document.querySelector("h1")
+btn.addEventListener("click",function(){
+    h1.innerHTML="I am clicked";
+});
